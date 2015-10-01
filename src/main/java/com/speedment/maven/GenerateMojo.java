3019c7f0a7aa10b2afaae1dd0a9a1c5ff8891f15
minborg <minborg@speedment.com>
2015-10-01 13:41:12 -0700

    Bump version and fix version bug

--- src/main/java/com/speedment/maven/GenerateMojo.java
+++ src/main/java/com/speedment/maven/GenerateMojo.java
@@ -19,7 +19,7 @@ package com.speedment.maven;
 import com.speedment.config.Project;
 import com.speedment.internal.core.code.MainGenerator;
 import com.speedment.internal.core.config.utils.GroovyParser;
-import com.speedment.internal.core.platform.component.Component;
+import com.speedment.component.Component;
 import java.io.File;
 import java.io.IOException;
 import org.apache.maven.plugin.MojoExecutionException;
